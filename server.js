c3febe65d1430837b631443654a6936c63ea8321
sircloubay <bayu.firmansyah@mail.sirclo.com>
2022-06-19 18:59:27 +0700

    add session feature

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
+const session = require('express-session');
+const config_session = require('./app/config/session');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,6 +13,9 @@ app.use(morgan('development'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(path.join( __dirname, '/public/assets')))
+
+// session
+app.use(session(config_session));
 
 // ejs engine
 app.set('views', path.join( __dirname, 'app/views'));
